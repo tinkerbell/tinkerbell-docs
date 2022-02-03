9e70d5793b0f7f8bc3631f933bb6f137f8f54183
Manuel Mendez <mmendez@equinix.com>
2022-02-03 12:57:45 -0500

    Add nix-shell and direnv goodness
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

---
+++ shell.nix
@@ -0,0 +1,12 @@
+let _pkgs = import <nixpkgs> { };
+in { pkgs ? import (_pkgs.fetchFromGitHub {
+  owner = "NixOS";
+  repo = "nixpkgs";
+  #branch@date: 21.11@2021-12-02
+  rev = "21.11";
+  sha256 = "sha256-AjhmbT4UBlJWqxY0ea8a6GU2C2HdKUREkG43oRr3TZg=";
+}) { } }:
+
+with pkgs;
+
+mkShell { buildInputs = [ nodePackages.prettier python3Packages.poetry ]; }
